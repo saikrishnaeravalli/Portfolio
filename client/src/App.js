8eb147435338af27c71dab900392026566d389c4
Sai Krishna Eravalli <seravall@asu.edu>
2023-09-22 13:01:05 -0700

    Added Visitor Count Function

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import About from './components/About';
 import Projects from './components/Projects';
 import ContactPage from './components/Contact';
 import Home from './components/Home';
+import axios from 'axios';
 
 function App() {
+
+  useEffect(() => {
+    // Send a request to the server to record the visit
+    axios.post('/api/record-visit')
+      .catch(error => console.error('Error recording visit:', error));
+  }, []);
+
   return (
     <ChakraProvider>
       <Home />
